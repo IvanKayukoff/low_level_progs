525094bfe645ecfa9f8abcaaaac5541d35e4fd36
Sky4784 <ivan4784@mail.ru>
2018-11-06 04:35:36 +0300

    pass filename through the cli parameter, also add report.tex

--- lab5/main.c
+++ lab5/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <memory.h>
 #include "bmp_wrapper.h"
 
 int print_read_status(bmp_read_status status) {
-    if      (status == READ_INVALID_SIGNATURE)   printf("Wrong BMP signature\n");
-    else if (status == READ_INVALID_BITS)        printf("BMP must be 24 bits per pixel\n");
-    else if (status == READ_INVALID_BMP_FORMAT)  printf("BMP must be in the V5 format\n");
-    else if (status == READ_ERROR)               printf("Error while reading, maybe file is missing\n");
+    if      (status == READ_INVALID_SIGNATURE)   fprintf(stderr, "Wrong BMP signature\n");
+    else if (status == READ_INVALID_BITS)        fprintf(stderr, "BMP must be 24 bits per pixel\n");
+    else if (status == READ_INVALID_BMP_FORMAT)  fprintf(stderr, "BMP must be in the V5 format\n");
+    else if (status == READ_ERROR)               fprintf(stderr, "Error while reading, maybe file is missing\n");
 
     if (status != READ_OK)  return 1;
     else                    return 0;
@@ -14,18 +15,24 @@ int print_read_status(bmp_read_status status) {
 
 int print_write_status(bmp_write_status status) {
     if (status == WRITE_ERROR) {
-        printf("Error while writing, permission denied");
+        fprintf(stderr, "Error while writing, probably file is missing");
         return 1;
     } else return 0;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc <= 1) {
+        fprintf(stderr, "This program requires the path to the bmp file as the first cli parameter\n");
+        return 1;
+    }
+    char *filename = argv[1];
+
     bmp_header *header = NULL;
-    bmp_read_status rh_status = read_header("resources/meme.bmp", &header);
+    bmp_read_status rh_status = read_header(filename, &header);
     if (print_read_status(rh_status) != 0) return 1;
 
     bmp_image *img = NULL;
-    bmp_read_status rp_status = read_bmp_image("resources/meme.bmp", &img);
+    bmp_read_status rp_status = read_bmp_image(filename, &img);
     if (print_read_status(rp_status) != 0) {
         free(header);
         return 1;
@@ -33,7 +40,9 @@ int main() {
 
     bmp_image *rotated = rotate_right(img);
 
-    bmp_write_status w_status = write_bmp_image(rotated, header, "resources/rotated.bmp");
+    char *out_filename = ".rotated.bmp";
+    strcat(filename, out_filename);
+    bmp_write_status w_status = write_bmp_image(rotated, header, filename);
     int is_write_failed = 0;
     if (print_write_status(w_status) != 0) is_write_failed = 1;
 
